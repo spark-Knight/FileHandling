5e21b30601fdbe027f0e730226dbea9135479482
Sid <dna8377850@gmail.com>
2021-12-22 17:28:20 +0530

    created some new Program

--- Handling.py
+++ Handling.py
@@ -1,4 +1,99 @@
-<<<<<<< HEAD
+# This Python program is use to write data to file.
+def WriteData(FileName,Data):
+    with open(FileName,"a+") as fOut:
+        fOut.write(Data + "\n")
+
+WriteData("Main.txt","My program is Running...")        
+
+
+# ////////////////////////////////////////////////////////////////#
+
+# Python program to create a new file in another directory.
+import os
+
+def AddFile(FileName,Path):
+    filename = FileName
+    Route = Path
+    Fullintro = os.path.join(Route, filename)
+    open(Fullintro, "w")
+
+
+AddFile("Mozilla.txt","C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad") 
+
+
+# ////////////////////////////////////////////////////////////////#
+
+# Append content to a file in Python.
+
+def AppendContents(filename, content):
+    file = open(filename, "a+")
+    file.write(content)
+    file.close()
+
+AppendContents("Main.txt","My Append Content program is working......")    
+
+# /////////////////////////////////////////////////////////////////#
+
+# Read contents of the file using readline() method in Python.
+def Read(filename):
+    file = open(filename + ".txt","r") 
+    data = file.readline()
+    while data:
+        print(data)
+        data = file.readline()
+    file.close()
+
+
+Read("Main.txt",)        
+
+# /////////////////////////////////////////////////////////////////#
+
+# Read contents of a file using readline() method and manipulating it in Python
+def Manipulation(filename):
+    file = open(filename + ".txt","r")
+    data = len(file.readlines())
+    while data:
+        print(data)
+        data = len(file.readline())
+
+Manipulation("Main.txt")
+ 
+# /////////////////////////////////////////////////////////////////#
+
+# Copy contents from one file to another file in Python.
+
+def CopyPaste(copyfilename,pastefilename):
+    with open(copyfilename) as fObj:
+        data = fObj.read()
+
+    with open(pastefilename,"a+") as fObj:
+        fObj.write(data)
+        print(f"I have Copy data from {copyfilename} and pasted to {pastefilename}.")
+
+CopyPaste("Main.txt","hello.txt")
+
+# /////////////////////////////////////////////////////////////////#
+
+# Copy odd lines of one file to another file in Python
+
+def CopyPasteOddLines(copyfilename,pastefilename):
+    copyfile = open(copyfilename, "r+")
+    pastefile= open(pastefilename, "w+")
+    count = copyfile.readlines()
+    for i in range(0,len(count)):
+        if(i % 2 == 0):
+            pastefile.write(count[i])
+        else:
+            pass
+
+
+    pastefile.close()
+    pastefile = open(pastefilename, "r")
+    count1 = pastefile.read()
+    print(count1)
+
+CopyPasteOddLines("hello.txt", "Main.txt")    
+
 # This Python program is use to write data to file.
 def WriteData(FileName,Data):
     with open(FileName,"a+") as fOut:
@@ -90,8 +185,7 @@ def CopyPasteOddLines(copyfilename,pastefilename):
     print(count1)
 
 CopyPasteOddLines("hello.txt", "Main.txt")    
-=======
-<<<<<<< HEAD
+
 # This Python program is use to write data to file.
 def WriteData(FileName,Data):
     with open(FileName,"a+") as fOut:
@@ -183,97 +277,117 @@ def CopyPasteOddLines(copyfilename,pastefilename):
     print(count1)
 
 CopyPasteOddLines("hello.txt", "Main.txt")    
-=======
-# This Python program is use to write data to file.
-def WriteData(FileName,Data):
-    with open(FileName,"a+") as fOut:
-        fOut.write(Data + "\n")
 
-WriteData("Main.txt","My program is Running...")        
+#//////////////////////////////////////////////////////////////////#
 
+# Count the total number of uppercase characters in a file in Python
 
-# ////////////////////////////////////////////////////////////////#
-
-# Python program to create a new file in another directory.
-import os
-
-def AddFile(FileName,Path):
-    filename = FileName
-    Route = Path
-    Fullintro = os.path.join(Route, filename)
-    open(Fullintro, "w")
-
-
-AddFile("Mozilla.txt","C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad") 
-
-
-# ////////////////////////////////////////////////////////////////#
-
-# Append content to a file in Python.
-
-def AppendContents(filename, content):
-    file = open(filename, "a+")
-    file.write(content)
+def TotalUppercase(Filename):
+    file = open(Filename, "r")
+    data = file.read()
+    count = 0
+    for letter in data:
+        if letter.isupper():
+            count+=1
+    print(count)
     file.close()
 
-AppendContents("Main.txt","My Append Content program is working......")    
+TotalUppercase("Main.txt") 
 
-# /////////////////////////////////////////////////////////////////#
+# //////////////////////////////////////////////////////////////////#
 
-# Read contents of the file using readline() method in Python.
-def Read(filename):
-    with open(filename) as fObj:
-        data = fObj.read()
-        print(data)
+# Python program to count total number of uppercase and lowercase characters in file
 
 
-Read("Main.txt",)        
-
-# /////////////////////////////////////////////////////////////////#
-
-# Read contents of a file using readline() method and manipulating it in Python
-def Manipulation(filename):
-    with open(filename) as fObj:
-        data = fObj.read()
-        print(data)
-        print(f"Number of Characters in {filename} " + str(len(data)))
-
-Manipulation("Main.txt")
- 
-# /////////////////////////////////////////////////////////////////#
-
-# Copy contents from one file to another file in Python.
-
-def CopyPaste(copyfilename,pastefilename):
-    with open(copyfilename) as fObj:
-        data = fObj.read()
-
-    with open(pastefilename,"a+") as fObj:
-        fObj.write(data)
-        print(f"I have Copy data from {copyfilename} and pasted to {pastefilename}.")
-
-CopyPaste("Main.txt","hello.txt")
-
-# /////////////////////////////////////////////////////////////////#
-
-# Copy odd lines of one file to another file in Python
-
-def CopyPasteOddLines(copyfilename,pastefilename):
-    copyfile = open(copyfilename, "r+")
-    pastefile= open(pastefilename, "w+")
-    count = copyfile.readlines()
-    for i in range(0,len(count)):
-        if(i % 2 == 0):
-            pastefile.write(count[i])
-        else:
-            pass
 
 
-    pastefile.close()
-    pastefile = open(pastefilename, "r")
-    count1 = pastefile.read()
-    print(count1)
+def TotalUpperAndLowercase(Filename):
+    file = open(Filename, "r")
+    data = file.read()
+    countup = 0
+    countlo = 0
+    for letter in data:
+        if letter.isupper():
+            countup+=1
+        elif letter.islower():
+            countlo+=1   
+    print(countup + countlo)
+    file.close()
 
-CopyPasteOddLines("hello.txt", "Main.txt")    
->>>>>>> c67a96dda3fdcc44463f4d331b4703df3b984a5c
->>>>>>> 2927a0e (What's the hell It's working)
+
+TotalUppercase("Main.txt")  
+
+# //////////////////////////////////////////////////////////////////#
+
+# Python program to delay printing of lines from a file using sleep function
+
+import time
+
+def TimeDelay(FileName):
+    file =  open(FileName,"r")
+    while(True):
+            File = file.read(1)
+            if (File == ""):
+                break
+            print(File, end = "")
+            time.sleep(0.50)
+    file.close()
+
+
+TimeDelay("MySelf.txt")
+
+#///////////////////////////////////////////////////////////////////#
+
+# Python program to count the number of lines in a file
+
+def Linecount(filename):
+    file = open(filename + '.txt', 'r')
+    Counter = 0
+    Read = file.read()
+    countlist = Read.split('\n')
+    for i in countlist:
+        if i :
+            Counter += 1
+    print(f"Number of Lines In {filename} is " + str(Counter))
+
+Linecount("MySelf")
+
+#///////////////////////////////////////////////////////////////////#
+
+# Python program to check a file's status in file Handling
+
+def status(filename):
+    file = open(filename + ".txt", 'rb')
+    FileName = file.name
+    Fileclosedoropen = file.closed
+    FileMode = file.mode
+    print(FileName)
+    print(Fileclosedoropen)
+    print(FileMode)
+    file.close()
+
+    Fileclosedoropen = file.closed
+    print("")
+    print(Fileclosedoropen)
+    
+status("MySelf") 
+
+#///////////////////////////////////////////////////////////////////#
+
+# Python program to delete a file
+
+import os
+def delete(filename):
+    os.remove(filename)
+    print(f"{filename} is deleted")
+
+delete(".txt")
+
+
+
+
+
+
+
+
+
