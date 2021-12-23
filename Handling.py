abab4e894b6879f6764a131670342f610690636b
Sid <dna8377850@gmail.com>
2021-12-23 15:21:16 +0530

    Remove Some Repeated Program

--- Handling.py
+++ Handling.py
@@ -4,33 +4,7 @@ def WriteData(FileName,Data):
         fOut.write(Data + "\n")
 
 WriteData("Main.txt","My program is Running...")        
-
-
-# ////////////////////////////////////////////////////////////////#
-
-# Python program to create a new file in another directory.
-import os
-
-def AddFile(FileName,Path):
-    filename = FileName
-    Route = Path
-    Fullintro = os.path.join(Route, filename)
-    open(Fullintro, "w")
-
-
-AddFile("Mozilla.txt","C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad") 
-
-
-# ////////////////////////////////////////////////////////////////#
-
-# Append content to a file in Python.
-
-def AppendContents(filename, content):
-    file = open(filename, "a+")
-    file.write(content)
-    file.close()
-
-AppendContents("Main.txt","My Append Content program is working......")    
+ 
 
 # /////////////////////////////////////////////////////////////////#
 
@@ -57,20 +31,6 @@ def Manipulation(filename):
         data = len(file.readline())
 
 Manipulation("Main.txt")
- 
-# /////////////////////////////////////////////////////////////////#
-
-# Copy contents from one file to another file in Python.
-
-def CopyPaste(copyfilename,pastefilename):
-    with open(copyfilename) as fObj:
-        data = fObj.read()
-
-    with open(pastefilename,"a+") as fObj:
-        fObj.write(data)
-        print(f"I have Copy data from {copyfilename} and pasted to {pastefilename}.")
-
-CopyPaste("Main.txt","hello.txt")
 
 # /////////////////////////////////////////////////////////////////#
 
@@ -93,15 +53,7 @@ def CopyPasteOddLines(copyfilename,pastefilename):
     print(count1)
 
 CopyPasteOddLines("hello.txt", "Main.txt")    
-
-# This Python program is use to write data to file.
-def WriteData(FileName,Data):
-    with open(FileName,"a+") as fOut:
-        fOut.write(Data + "\n")
-
-WriteData("Main.txt","My program is Running...")        
-
-
+  
 # ////////////////////////////////////////////////////////////////#
 
 # Python program to create a new file in another directory.
@@ -114,8 +66,7 @@ def AddFile(FileName,Path):
     open(Fullintro, "w")
 
 
-AddFile("Mozilla.txt","C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad") 
-
+AddFile("Mozilla.txt","C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad")      
 
 # ////////////////////////////////////////////////////////////////#
 
@@ -127,28 +78,6 @@ def AppendContents(filename, content):
     file.close()
 
 AppendContents("Main.txt","My Append Content program is working......")    
-
-# /////////////////////////////////////////////////////////////////#
-
-# Read contents of the file using readline() method in Python.
-def Read(filename):
-    with open(filename) as fObj:
-        data = fObj.read()
-        print(data)
-
-
-Read("Main.txt",)        
-
-# /////////////////////////////////////////////////////////////////#
-
-# Read contents of a file using readline() method and manipulating it in Python
-def Manipulation(filename):
-    with open(filename) as fObj:
-        data = fObj.read()
-        print(data)
-        print(f"Number of Characters in {filename} " + str(len(data)))
-
-Manipulation("Main.txt")
  
 # /////////////////////////////////////////////////////////////////#
 
@@ -162,121 +91,7 @@ def CopyPaste(copyfilename,pastefilename):
         fObj.write(data)
         print(f"I have Copy data from {copyfilename} and pasted to {pastefilename}.")
 
-CopyPaste("Main.txt","hello.txt")
-
-# /////////////////////////////////////////////////////////////////#
-
-# Copy odd lines of one file to another file in Python
-
-def CopyPasteOddLines(copyfilename,pastefilename):
-    copyfile = open(copyfilename, "r+")
-    pastefile= open(pastefilename, "w+")
-    count = copyfile.readlines()
-    for i in range(0,len(count)):
-        if(i % 2 == 0):
-            pastefile.write(count[i])
-        else:
-            pass
-
-
-    pastefile.close()
-    pastefile = open(pastefilename, "r")
-    count1 = pastefile.read()
-    print(count1)
-
-CopyPasteOddLines("hello.txt", "Main.txt")    
-
-# This Python program is use to write data to file.
-def WriteData(FileName,Data):
-    with open(FileName,"a+") as fOut:
-        fOut.write(Data + "\n")
-
-WriteData("Main.txt","My program is Running...")        
-
-
-# ////////////////////////////////////////////////////////////////#
-
-# Python program to create a new file in another directory.
-import os
-
-def AddFile(FileName,Path):
-    filename = FileName
-    Route = Path
-    Fullintro = os.path.join(Route, filename)
-    open(Fullintro, "w")
-
-
-AddFile("Mozilla.txt","C:\\Users\\dna83\\OneDrive\\Desktop\\Your dad") 
-
-
-# ////////////////////////////////////////////////////////////////#
-
-# Append content to a file in Python.
-
-def AppendContents(filename, content):
-    file = open(filename, "a+")
-    file.write(content)
-    file.close()
-
-AppendContents("Main.txt","My Append Content program is working......")    
-
-# /////////////////////////////////////////////////////////////////#
-
-# Read contents of the file using readline() method in Python.
-def Read(filename):
-    with open(filename) as fObj:
-        data = fObj.read()
-        print(data)
-
-
-Read("Main.txt",)        
-
-# /////////////////////////////////////////////////////////////////#
-
-# Read contents of a file using readline() method and manipulating it in Python
-def Manipulation(filename):
-    with open(filename) as fObj:
-        data = fObj.read()
-        print(data)
-        print(f"Number of Characters in {filename} " + str(len(data)))
-
-Manipulation("Main.txt")
- 
-# /////////////////////////////////////////////////////////////////#
-
-# Copy contents from one file to another file in Python.
-
-def CopyPaste(copyfilename,pastefilename):
-    with open(copyfilename) as fObj:
-        data = fObj.read()
-
-    with open(pastefilename,"a+") as fObj:
-        fObj.write(data)
-        print(f"I have Copy data from {copyfilename} and pasted to {pastefilename}.")
-
-CopyPaste("Main.txt","hello.txt")
-
-# /////////////////////////////////////////////////////////////////#
-
-# Copy odd lines of one file to another file in Python
-
-def CopyPasteOddLines(copyfilename,pastefilename):
-    copyfile = open(copyfilename, "r+")
-    pastefile= open(pastefilename, "w+")
-    count = copyfile.readlines()
-    for i in range(0,len(count)):
-        if(i % 2 == 0):
-            pastefile.write(count[i])
-        else:
-            pass
-
-
-    pastefile.close()
-    pastefile = open(pastefilename, "r")
-    count1 = pastefile.read()
-    print(count1)
-
-CopyPasteOddLines("hello.txt", "Main.txt")    
+CopyPaste("Main.txt","hello.txt") 
 
 #//////////////////////////////////////////////////////////////////#
 
@@ -299,8 +114,6 @@ TotalUppercase("Main.txt")
 # Python program to count total number of uppercase and lowercase characters in file
 
 
-
-
 def TotalUpperAndLowercase(Filename):
     file = open(Filename, "r")
     data = file.read()
@@ -315,7 +128,7 @@ def TotalUpperAndLowercase(Filename):
     file.close()
 
 
-TotalUppercase("Main.txt")  
+TotalUpperAndLowercase("Main.txt")  
 
 # //////////////////////////////////////////////////////////////////#
 
@@ -381,7 +194,7 @@ def delete(filename):
     os.remove(filename)
     print(f"{filename} is deleted")
 
-delete(".txt")
+delete(".txt")  
 
 
 
